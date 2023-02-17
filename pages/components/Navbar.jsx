60afd1e6ad9a29e5b37d8cfd189e91f691fbc38e
farayzk <aldifahriziilmawan@gmail.com>
2023-02-17 17:26:58 +0700

    fix and add

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -3,14 +3,15 @@ import Link from 'next/link';
 import { useMetamask, useAddress } from '@thirdweb-dev/react';
 const Navbar = (metamaskWallet, address) => {
   return (
-    <header className="h-20 py-12 flex items-center justify-between pr-20 pl-5 bg-slate-500">
+    <header className="h-16 py-12 flex items-center justify-between pr-20 bg-pink-500">
       <div className="logo items-center flex ml-10">
-        <div className="grid-cols-3">
-          <h2 className="w-22 h-12 text-white">Ticketing NFT</h2>
-          <p className="ml-3 text-white text-[10px]">
-            Powered By
-            <span className="text-white font-bold italic">Cherry Labs</span>
-          </p>
+        <div className="grid grid-cols-2 items-center">
+          <div className="bg-white w-14 h-14"></div>
+            <h2 className="w-22 h-12 text-white">Ticketing NFT</h2>
+            <p className="text-white text-[10px]">
+              Powered By
+              <span className="text-white font-bold italic"> Cherry Labs</span>
+            </p>
         </div>
       </div>
       <nav className="mr-32">
---
+++ pages/components/Partner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Partner = () => {
+    return(
+        <p>Partner</p>
+    );
+}
+
+export default Partner
